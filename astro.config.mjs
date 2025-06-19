4e0cd296ea87436f605f1467065ca0e1a880c652
doducdungvn <apeo.d3@gmail.com>
2025-06-20 01:37:38 +0700

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import content from "@astrojs/content";
 
 export default defineConfig({
   output: "static",
-  integrations: [mdx(), content()],
+  integrations: [mdx()],
 });
