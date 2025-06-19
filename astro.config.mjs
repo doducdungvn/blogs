d799a2fc5f5a362169a46f58b8cb237eaefe5436
doducdungvn <apeo.d3@gmail.com>
2025-06-20 00:17:26 +0700

    Clean: xoá @astrojs/content, giữ lại cấu hình mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import content from "@astrojs/content";
 
 export default defineConfig({
-  integrations: [mdx(), content()],
-  markdown: { shikiConfig: { theme: 'dracula' } }
+  integrations: [mdx()],
 });
